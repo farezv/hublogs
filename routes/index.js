38c9c700fb257ad7431af4071cd4619f19e6904f
Farez Vadsaria <farezvad@gmail.com>
2014-09-14 03:59:03 -0400

    Getting valid json from github api
    Used mikeal/request module to retrieve the relevant json from github’s
    api

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,43 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Find a GitHub user\'s blog' });
+  res.render('hubuser', { title: 'Find a GitHub user\'s blog' });
 });
 
-/* GET Hello World page. */
-router.get('/helloworld', function(req, res) {
-	res.render('helloworld', { title: 'Hello World!'})
+/* GET Blog results page. */
+router.get('/blogs', function(req, res) {
+	res.render('blogs', { title: 'Here\'s the blog link!'})
+});
+
+/* POST to Find Blogs */
+router.post('/findblogs', function(req, res) {
+	// Build the search url
+	var searchUrl = 'https://api.github.com/users/' + req.body.username;
+	console.log('searchUrl: ' + searchUrl);
+	// Access the api and retrieve the JSON
+	request({
+			url: searchUrl,
+			headers: {
+				'User-Agent': 'request'
+			}
+		}, function(error, response, body) {
+	// Parse the JSON to find blog url
+		if(!error && response.statusCode == 200) {
+			console.log(JSON.parse(body));
+			res.location('blogs');
+			res.redirect('blogs');
+		} else {
+			// Deal with error case where url can't be found
+			console.log(error);
+			res.render('error', { title: 'Oops, something went wrong!'});
+		}
+	});	
+	// Present the url
+
+
 });
 
 module.exports = router;
