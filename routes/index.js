fc72bf4805638ec6216dccbee2201edc0d4cbb3b
Farez Vadsaria <farezvad@gmail.com>
2014-09-20 21:02:26 -0400

    Simple url generation works
    Input Github user’s handle. Output is a link to their website/blog

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('hubuser', { title: 'Find a GitHub user\'s blog' });
+  res.render('hubuser', { title: 'Find a GitHub user\'s blog/website' });
 });
 
 /* GET Blog results page. */
-router.get('/blogs', function(req, res) {
-	res.render('blogs', { title: 'Here\'s the blog link!'})
+router.get('/blogs/:blogUrl?', function(req, res) {
+	if(req.params.blogUrl) {
+		var blogUrl = req.params.blogUrl;
+		console.log(blogUrl);
+		res.render('blogs', { blogLink: blogUrl });
+	} else res.render('blogs', { title: 'Couldn\'t find the blog link :('});
 });
 
 /* POST to Find Blogs */
@@ -24,20 +28,32 @@ router.post('/findblogs', function(req, res) {
 				'User-Agent': 'request'
 			}
 		}, function(error, response, body) {
-	// Parse the JSON to find blog url
+		// Parse the JSON to find blog url and clean it up
 		if(!error && response.statusCode == 200) {
-			console.log(JSON.parse(body));
-			res.location('blogs');
-			res.redirect('blogs');
+			var blogUrl = JSON.parse(body).blog;
+			blogUrl = urlCleanup(blogUrl)
+			blogUrl = encodeURIComponent(blogUrl);
+			res.redirect('blogs/' + blogUrl);
 		} else {
-			// Deal with error case where url can't be found
+		// Deal with error case where url can't be found
 			console.log(error);
 			res.render('error', { title: 'Oops, something went wrong!'});
 		}
 	});	
-	// Present the url
-
-
 });
+
+function urlCleanup(url) {
+	if(url.substring(0,5) == 'https') {
+		url = url.substring(8);
+		console.log(url);
+	} else if(url.substring(0,4) == 'http') {
+		url = url.substring(7);
+		console.log(url);
+	}
+	if(url.substring(url.length - 1) == '/') {
+		url = url.substring(0, url.length - 1);
+	}
+	return url;
+}
 
 module.exports = router;
